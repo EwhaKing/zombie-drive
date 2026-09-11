f63a8d2c1478650235c952c4fbfd969b9e41428a
정연재 <amyjeong1022@ewha.ac.kr>
2026-09-12 03:37:00 +0900

    Add: 타이머 기능 추가

--- Assets/Scripts/MiniGame/RepairShop/RepairShopScreenManager.cs
+++ Assets/Scripts/MiniGame/RepairShop/RepairShopScreenManager.cs
@@ -4,11 +4,15 @@ public class RepairShopScreenManager : MonoBehaviour
 {
     [SerializeField] private GameObject whitePanel;
     [SerializeField] private WheelRemovalMiniGame wheelMiniGame;
+    [SerializeField] private RepairShopGameManager gameManager;
 
     public void OpenWhiteScreen()
     {
         whitePanel.SetActive(true);
+
         wheelMiniGame.ResetMiniGame();
+
+        gameManager.BeginRepair();
     }
 
     public void CloseWhiteScreen()
